059c32ae1c37839c7fab07052b95dda424f10dd2
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-11-28 11:38:03 +0530

    Added stateOfWorld function and tests

--- src/lib.js
+++ src/lib.js
@@ -11,4 +11,16 @@ const createBoard = function(size){
   return board;
 }
 
-module.exports = { createBoard };
+const stateOfWorld = function(size,inputdata){
+  let grid = createBoard(size);
+  for(let index = 0; index < size; index++){
+    for(let j = 0; j < inputdata.length; j++){
+      let position = grid[index].indexOf(inputdata[j])
+      if(position !== -1){
+        grid[index][position] = 'A';
+      }
+    }
+  }
+  return grid;
+}
+module.exports = { createBoard,stateOfWorld };
--- test/libTest.js
+++ test/libTest.js
@@ -1,5 +1,5 @@
 const { equal,deepEqual } = require('assert');
-const { createBoard } = require('../src/lib.js');
+const { createBoard,stateOfWorld } = require('../src/lib.js');
 
 describe( 'createBoard()', function(){
   it( 'should return empty array for size 0', function(){
@@ -8,5 +8,14 @@ describe( 'createBoard()', function(){
   it('should return a nXn array for given size :n', function(){
     deepEqual(createBoard(1),[[0]]);
     deepEqual(createBoard(2),[[0,1],[2,3]]);
+  });
+});
+
+describe( 'stateOfWorld()', function(){
+  it('should return empty array for size 0', function(){
+    deepEqual(stateOfWorld(0,[]),[]);
+  });
+  it('should return an array representing the states for given size :n and inputdata for alive elements',  function(){
+    deepEqual(stateOfWorld(2,[0,1]),[['A','A'],[2,3]]);
   });
 });
